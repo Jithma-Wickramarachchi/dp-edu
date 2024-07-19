d1f621a9688b4f791a36e1a22f7f3179dcb6b613
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-07-19 06:50:18 +0530

    first commit

---
+++ my-npm-project/module-demo.js
@@ -0,0 +1,6 @@
+let ascii_text_generator = require('ascii-text-generator')
+
+let input_text = "Node Express Project"
+let ascii_text = ascii_text_generator(input_text, "5")
+
+console.log(ascii_text)
